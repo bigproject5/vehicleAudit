adbcc42177164042ab9abae39a8eb8ccac78541e
PARK HYEONGMIN <nsby8882@naver.com>
2025-07-24 17:24:20 +0900

    spring boot init

---
+++ src/main/java/aivle/project/vehicleAduit/VehicleAduitApplication.java
@@ -0,0 +1,13 @@
+package aivle.project.vehicleAduit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleAduitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleAduitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aivle/project/vehicleAduit/VehicleAduitApplicationTests.java
@@ -0,0 +1,13 @@
+package aivle.project.vehicleAduit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleAduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
