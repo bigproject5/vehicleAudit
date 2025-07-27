8e92720890885ecd37b12dd6baf62054707918d7
김강년 <64246800+ekek54@users.noreply.github.com>
2025-07-28 01:06:29 +0900

    Feat: JPA 감사 기능 활성화

--- src/main/java/aivle/project/vehicleAudit/VehicleAuditApplication.java
+++ src/main/java/aivle/project/vehicleAudit/VehicleAuditApplication.java
@@ -2,8 +2,10 @@ package aivle.project.vehicleAudit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class VehicleAuditApplication {
 
 	public static void main(String[] args) {
