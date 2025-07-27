57d50fca3bfaf2424308a7aea3d50141704e248e
김강년 <64246800+ekek54@users.noreply.github.com>
2025-07-28 01:07:28 +0900

    Feat: Task 엔티티 추가

---
+++ src/main/java/aivle/project/vehicleAudit/domain/Task.java
@@ -0,0 +1,37 @@
+package aivle.project.vehicleAudit.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_id", nullable = false)
+    private Long id;
+
+    @Column(name = "worker_id", nullable = false)
+    private Long workerId;
+
+    @Column(name = "worker_name", nullable = false)
+    private String workerName;
+
+    @CreatedDate
+    @Column(name = "started_at", nullable = false)
+    private LocalDateTime startedAt;
+
+    @Column(name = "finished_at", nullable = true)
+    private LocalDateTime finishedAt;
+
+    @Column(name = "resolve", nullable = true, length = 300)
+    private String resolve;
+}
