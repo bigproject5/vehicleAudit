827802dacce29fa47864787c85f3906e79dc0ef9
김강년 <64246800+ekek54@users.noreply.github.com>
2025-07-28 00:28:32 +0900

    refactor: AuditStatus 클래스 리팩토링 - description 필드 제거 - enum 값 간소화

--- src/main/java/aivle/project/vehicleAudit/domain/enumerate/AuditStatus.java
+++ src/main/java/aivle/project/vehicleAudit/domain/enumerate/AuditStatus.java
@@ -1,17 +1,7 @@
 package aivle.project.vehicleAudit.domain.enumerate;
 
-import lombok.Getter;
-
-@Getter
 public enum AuditStatus {
-    IN_DIAGNOSIS("진단 중"),
-    IN_PROGRESS("처리 중"),
-    COMPLETED("완료");
-
-    private final String description;
-
-    AuditStatus(String description) {
-        this.description = description;
-    }
-
+    IN_DIAGNOSIS,
+    IN_PROGRESS,
+    COMPLETED;
 }
