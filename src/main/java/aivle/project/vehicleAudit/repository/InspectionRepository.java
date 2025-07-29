3d21ec647229e42c3879acad67ab70ac55a59ded
김강년 <64246800+ekek54@users.noreply.github.com>
2025-07-29 13:56:36 +0900

    Feat: 추가 InspectionRepository 메서드 구현
    작업자 ID로 Inspection을 페이징하여 조회하는 메서드와
    Inspection ID로 작업을 포함하여 조회하는 메서드를 추가했습니다.

--- src/main/java/aivle/project/vehicleAudit/repository/InspectionRepository.java
+++ src/main/java/aivle/project/vehicleAudit/repository/InspectionRepository.java
@@ -1,10 +1,19 @@
 package aivle.project.vehicleAudit.repository;
 
 import aivle.project.vehicleAudit.domain.Inspection;
+
+import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface InspectionRepository extends JpaRepository<Inspection, Long> {
 
     List<Inspection> findByAuditId(Long auditId);
+    Page<Inspection> findByTaskWorkerId(Long workerId, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"task"})
+    Optional<Inspection> findWithTaskById(Long inspectionId);
 }
