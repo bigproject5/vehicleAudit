719ae1b8b62c62499bcf211eab18988fa4c65e63
김강년 <64246800+ekek54@users.noreply.github.com>
2025-07-31 13:08:44 +0900

    Feat: 추가 QuerydslPredicateExecutor 인터페이스 구현
    InspectionRepository에 QuerydslPredicateExecutor를 추가하여
    동적 쿼리 생성을 지원합니다.

--- src/main/java/aivle/project/vehicleAudit/repository/InspectionRepository.java
+++ src/main/java/aivle/project/vehicleAudit/repository/InspectionRepository.java
@@ -7,9 +7,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import java.util.List;
 
-public interface InspectionRepository extends JpaRepository<Inspection, Long> {
+public interface InspectionRepository extends JpaRepository<Inspection, Long>, QuerydslPredicateExecutor<Inspection> {
 
     List<Inspection> findByAuditId(Long auditId);
     Page<Inspection> findByTaskWorkerId(Long workerId, Pageable pageable);
