1bfebc08c12f1bf38c2096aad512598b09ca7b16
김강년 <64246800+ekek54@users.noreply.github.com>
2025-08-14 23:15:15 +0900

    Refactor: inspection 관련 메서드명 변경

--- src/main/java/aivle/project/vehicleAudit/rest/AuditController.java
+++ src/main/java/aivle/project/vehicleAudit/rest/AuditController.java
@@ -34,7 +34,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -136,7 +135,7 @@ public class AuditController {
     }
 
     @PatchMapping("/inspections/{inspectionId}")
-    public ResponseEntity<ResponseDTO<?>> startTaskOnInspection(
+    public ResponseEntity<ResponseDTO<?>> updateInspection(
             @PathVariable Long inspectionId,
             @RequestHeader(name = "X-User-Id") Long workerId,
             @RequestHeader(name = "X-User-Name") String workerName,
